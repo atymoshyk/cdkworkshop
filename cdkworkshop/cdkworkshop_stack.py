7a4e9967631aa80bba5176e528cd23fc155f7c86
Andrii Tymoshyk <andrii.tymoshyk@timeout.com>
2019-12-23 12:02:01 +0200

    run eks

--- cdkworkshop/cdkworkshop_stack.py
+++ cdkworkshop/cdkworkshop_stack.py
@@ -1,6 +1,8 @@
 from aws_cdk import (
     aws_lambda as _lambda,
     aws_apigateway as apigw,
+    aws_eks as eks,
+    aws_ec2 as ec2,
     core,
 )
 
@@ -10,14 +12,24 @@ class CdkworkshopStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # Define a Lambda resource
-        my_lambda = _lambda.Function(
-            self, 'HelloHandler',
-            runtime=_lambda.Runtime.PYTHON_3_7,
-            code=_lambda.Code.asset('lambda'),
-            handler='hello.handler',
-        )
+        # my_lambda = _lambda.Function(
+        #     self, 'HelloHandler',
+        #     runtime=_lambda.Runtime.PYTHON_3_7,
+        #     code=_lambda.Code.asset('lambda'),
+        #     handler='hello.handler',
+        # )
 
-        apigw.LambdaRestApi(
-            self, 'Endpoint',
-            handler=my_lambda,
+        # apigw.LambdaRestApi(
+        #     self, 'Endpoint',
+        #     handler=my_lambda,
+        # )
+        eks_cluster = eks.Cluster(
+            self, "andrii_test_cdk_cluster",
+            default_capacity=0,# disabled default capacity
+            default_capacity_instance=ec2.InstanceType("t2.small")
+        )
+        eks_cluster.add_capacity("frontend-nodes",
+            instance_type=ec2.InstanceType("t2.small"),
+            desired_capacity=2,
+            vpc_subnets={"subnet_type": ec2.SubnetType.PUBLIC}
         )
